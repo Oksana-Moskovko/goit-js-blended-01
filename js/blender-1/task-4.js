6b9efaa80101ac1d10f86efa608a53a7dd552202
Oksana <omoskovko98@gmail.com>
2025-03-16 13:03:47 +0200

    add 1-6 task

---
+++ js/blender-1/task-4.js
@@ -0,0 +1,8 @@
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+
+// let count = 0;
+
+// while (count <= 20) {
+//   console.log(`Count: ${count}`);
+//   count += 1;
+// }
---
+++ js/blender-1/task-5.js
@@ -0,0 +1,21 @@
+// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
+// мінімальне і максимальне число відповідно.
+// Напишіть цикл всередині функції, який виводить у консоль
+// всі числа від max до min за спаданням.
+// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
+// і поверніть її з функції.
+
+// function getNumbers(min, max) {
+//     let sum = 0;
+//     for (let index = max; index >= min; index--) {
+//         console.log(index);
+//         if (index % 2 === 0) {
+//             sum += index;
+//         }
+//     }
+//     return `Сума парних чисел ${sum}`;
+// }
+
+// console.log(getNumbers(0, 12))
+// console.log(getNumbers(5, 25))
+// console.log(getNumbers(0, 3))
